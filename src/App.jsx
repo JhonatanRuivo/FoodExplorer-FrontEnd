87e34ae64908e1256052e92b5b67daa4a2c597b8
Jhonatan <jhonatanruivo@outlook.com>
2023-07-24 22:08:59 -0300

    feat: ThemeProvider, GlobalStyles

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-import {Container} from './theme'
-
-export default function App() {
-    return (
-        <Container>
-            <h1>Title</h1>
-        </Container>
-    )
-}
---
+++ src/pages/SignIn/index.jsx
@@ -0,0 +1,13 @@
+import { Container } from "./styles";
+
+
+export function SignIn() {
+
+  return(
+    <Container>
+      <h1>Hello React!</h1>
+    </Container>
+    
+  )
+
+}
