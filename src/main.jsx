92da24beeb63aa9cf865a1e03c54018198d64a29
Jhonatan <jhonatanruivo@outlook.com>
2023-08-16 19:45:58 -0300

    feat: page MenuAdmin mobile

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,13 @@ import { ThemeProvider } from 'styled-components'
 import GlobalStyles from './styles/global'
 import theme from './styles/theme'
 
-import { EditDish } from './pages/EditDish'
+import { Menu } from './pages/Menu'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <EditDish />
+      <Menu />
     </ThemeProvider>
   </React.StrictMode>
 )
---
+++ src/pages/MenuAdmin/index.jsx
@@ -0,0 +1,24 @@
+import { Container, Main, Content, Items } from './styles'
+import { IoSearchOutline } from 'react-icons/io5'
+
+import { HeaderMenu } from '../../components/HeaderMenu'
+import { ButtonText } from '../../components/ButtonText'
+import { Footer } from '../../components/Footer'
+import { Input } from '../../components/Input'
+
+export function MenuAdmin() {
+  return (
+    <Container>
+      <HeaderMenu />
+      <Main>
+        <Input id="input" icon={IoSearchOutline} placeholder="Busque por pratos ou ingredientes" />
+        <Content>
+          <Items></Items>
+        </Content>
+        <ButtonText title="Novo Prato" pop large border textStart />
+        <ButtonText title="Sair" pop large border textStart />
+      </Main>
+      <Footer />
+    </Container>
+  )
+}
