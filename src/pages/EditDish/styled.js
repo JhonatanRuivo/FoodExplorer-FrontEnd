90400b8f29bd6fef93aa67411974d6f82317a584
Jhonatan <jhonatanruivo@outlook.com>
2023-08-15 21:16:54 -0300

    fix: Pages NewDish and  EditDish position footer

--- src/pages/EditDish/styled.js
+++ src/pages/EditDish/styled.js
@@ -1,6 +1,16 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  min-height: 100vh;
+
+  padding-bottom: 193px;
+
+  display: flex;
+  flex-direction: column;
+  position: relative;
+
   .back {
     width: 106px;
     height: 34px;
